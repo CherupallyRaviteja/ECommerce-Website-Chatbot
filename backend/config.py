1355eed5440f877b3c7389f6bfa13439017d07cf
CherupallyRaviteja <cherupellyraviteja2005@gmail.com>
2026-06-29 15:31:46 +0530

    Gemini Embedding Model

--- backend/config.py
+++ backend/config.py
@@ -1,17 +1,19 @@
 import os
-import nltk
-from sentence_transformers import SentenceTransformer
 import dotenv
-nltk.download('punkt', quiet=True)
-import google.generativeai as genai
-import os
+import nltk
+from google import genai as new_genai
+from google.genai import types
+import google.generativeai as old_genai
 dotenv.load_dotenv()
+nltk.download("punkt", quiet=True)
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+old_genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-MODEL = genai.GenerativeModel("gemini-2.5-flash")
-EMBED_MODEL = 'all-MiniLM-L6-v2'
-EMBED_DIM = 384
+MODEL = old_genai.GenerativeModel("gemini-2.5-flash")
+
+client = new_genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+EMBED_DIM = 768
 SIM_THRESHOLD = 0.3
 FAISS_TOP_K = 4
 SENT_SIM_THRESHOLD = 0.7
@@ -22,17 +24,18 @@ POPPLER_BIN = r"C:\poppler\Library\bin"
 if POPPLER_BIN not in os.environ["PATH"]:
     os.environ["PATH"] = POPPLER_BIN + os.pathsep + os.environ["PATH"]
 
-embed_model = None
-
-import google.generativeai as genai
 
 def get_embedding(text):
-    response = genai.embed_content(
-        model="models/embedding-001",
-        content=text,
-        task_type="retrieval_document"
+    response = client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=text,
+        config=types.EmbedContentConfig(
+            task_type="RETRIEVAL_DOCUMENT",
+            output_dimensionality=EMBED_DIM,
+        ),
     )
-    return response["embedding"]
+    return response.embeddings[0].values
+
 
 if __name__ == "__main__":
     print("✅ Config loaded")
